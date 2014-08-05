c380cdbeba90ebc224a50bf26b4633d03339f279
Revati-Bhavsar <revati.bhavsar@cumminscollege.in>
2014-08-05 08:56:43 +0530

    Update hello.cpp
    added add() function;

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 using namespace std;
 
+
 void main()
 {
   cout<<"Hello World";
+  cout<<"\n";
  
 }
+void add()
+{
+  int a,b,c;
+  cout<<"Enter value of a & b";
+  
+}
+c=a+b
